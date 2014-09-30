a70cf1937962183b74495217bf20eab2c20cab02
paperleaf <paperleaf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-30 20:00:09 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/timespan/trunk@999403 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/ttr-options.php
@@ -0,0 +1,101 @@
+<div class="wrap">
+    <h2>TimeSpan</h2>   
+    <h3>TimeSpan Plugin Options</h3>
+
+    <h5>How to Use:</h5>
+    <p>
+        There are two different options for this plugin, one for custom theme developers, and one for the average wordpress user.
+        
+        <h4>If you are a custom theme developer follow these instructions:</h4>
+        <ul>
+            <li>- Fill out the options below</li>
+            <li>- Use the [time-span] shortcode in your custom theme templates.</li>
+        </ul>
+
+        <h4>If you want us to take care of the heavy lifting, follow these instructions:</h4>
+        <ul>
+            <li>- Fill out the options below</li>
+            <li>- Click the checkbox at the bottom labeled 'Automatically insert the time to read at the top of Posts'</li>
+            <li>- Sit back, relax, and enjoy.</li>
+        </ul>
+
+    </p>
+
+    <hr>
+
+
+    <form method="post" action="options.php">
+
+        <?php settings_fields( 'ttr-group' ); ?>
+
+        <?php do_settings_sections( 'ttr-group' ); ?>
+
+        <table class="form-table">
+
+            <tr valign="top">
+
+                <th scope="row">Average Reading Words Per Minute</th>
+                <th scrop="row">
+                    <p style="margin-top:-5px;">Enter the average reading speed, in WPM, for your readers (Defaults to 250wpm - North American average).</p>
+                    <p><em>eg. '220'</em></p>
+                </th>
+
+                <td style="vertical-align:top;"><input type="text" name="wpm" value="<?php echo get_option('wpm'); ?>" /></td>
+
+            </tr>
+
+            <tr valign="top">
+
+                <th scope="row">Less Than 1 Minute</th>
+                <th scrop="row">
+                    <p style="margin-top:-5px;">If the post takes less than one minute to read, this whole field will be displayed.</p>
+                    <p><em >eg. '<span style="text-decoration: underline;">less than 1 minute, friendo!</span></em>'</p>
+                </th>
+
+                <td style="vertical-align:top;"><input type="text" name="lt1" value="<?php echo get_option('lt1'); ?>" /></td>
+
+            </tr>
+
+            <tr valign="top">
+
+                <th scope="row">Greater Than 1 Minute Prefix</th>
+                <th scrop="row">
+                    <p style="margin-top:-5px;">If the post takes more than one minute to read, this field will be displayed before the number of minutes.</p>
+                    <p><em>eg. '<span style="text-decoration: underline;">greater than</span> 2 minutes, girlfriend!</em>'</p>
+                </th>
+
+                <td style="vertical-align:top;"><input type="text" name="gt1" value="<?php echo get_option('gt1'); ?>" /></td>
+
+            </tr>
+
+            <tr valign="top">
+
+                <th scope="row">Greater Than 1 Minute Postfix</th>
+                <th scrop="row">
+                    <p style="margin-top:-5px;">If the post takes more than one minute to read, this field will be displayed after the number of minutes.</p>
+                    <p><em>eg. 'greater than</span> 2 <span style="text-decoration: underline;">minutes, girlfriend!</em>'</p>
+                </th>
+
+                <td style="vertical-align:top;"><input type="text" name="gt1po" value="<?php echo get_option('gt1po'); ?>" /></td>
+
+            </tr>
+
+            <tr valign="top">
+
+                <th scope="row">Automatically Inject TimeSpan Code</th>
+                <th scrop="row">
+                    <p style="margin-top:-5px;">Checking off this box will make the plugin automatically display the TimeSpan code right before post content.</p>
+                </th>
+
+                <td style="vertical-align:top;"><input name="autots" type="checkbox" value="1" <?php checked( '1', get_option( 'autots' ) ); ?> />
+</td>
+
+            </tr>
+
+        </table>
+
+        <?php submit_button(); ?>
+
+    </form>
+
+</div>
---
+++ timespan.php
@@ -0,0 +1,124 @@
+<?php 
+/* 
+Plugin Name: TimeSpan
+Plugin URI: http://paper-leaf.com
+Description: A simple, easy to use plugin which can be used to display the average time it will take to read a post.
+Version: 1.0
+Author: Paper Leaf Design
+Author URI: http://paper-leaf.com
+License: GPLv2 or Later
+*/
+
+add_action( 'admin_menu', 'ttr_menu' );
+
+function ttr_menu() {
+	add_options_page( 'TimeSpan Options', 'TimeSpan', 'manage_options', 'ttr-identifier', 'ttr_options' );
+}
+
+
+add_action( 'admin_init', 'register_ttrsettings' );
+
+function register_ttrsettings() {
+	//register our settings
+    register_setting( 'ttr-group', 'wpm' );
+	register_setting( 'ttr-group', 'lt1' );
+    register_setting( 'ttr-group', 'gt1' );
+    register_setting( 'ttr-group', 'gt1po' );
+	register_setting( 'ttr-group', 'autots' );
+}
+
+function ttr_options() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+
+	include('includes/ttr-options.php');
+
+}
+
+
+add_shortcode('time-span', 'time_to_read');
+
+function time_to_read() {
+    $content = get_post_field( 'post_content', $post->ID );
+    $word_count = str_word_count( strip_tags( $content ) );
+    $words_per_minute = 250;
+    if (get_option('wpm') != '') {
+        $words_per_minute = get_option('wpm');
+    }
+    $time_in_minutes = $word_count/$words_per_minute;
+
+    if ($time_in_minutes < 1) {
+        $full = '<span class="timespan less-than"><p>';
+        if (get_option('lt1') != '') {
+            $full .= get_option('lt1'); // get the option from WP backend
+        } else {
+            $full .= 'less than one minute.';
+        }
+        $full .= '</p></span>';
+        // $full .= strval($time_in_minutes);
+        return $full;
+    } else {
+        $time_in_minutes = round($time_in_minutes); // round the time to a full number
+        $strTime = strval($time_in_minutes); // convert time to a string
+        $content = '<span class="timespan greater-than"><p>';
+        if (get_option('gt1') != '') {
+            $full .= get_option('gt1'); // get the option from WP backend
+        } else {
+            $full .= 'greater than';
+        }
+        $content .= '<span class="ts-time"> ' . $strTime . '</span> '; // add time onto the end of the content
+        if (get_option('gt1po') != '') {
+            $full .= get_option('gt1po'); // get the option from WP backend
+        } else {
+            $full .= 'minutes';
+        }
+        return $content; // return rounded time
+    }
+}
+
+function auto_TTR( $content ) {
+    if (get_option('autots') == 1) {    
+        if ( is_single() ) {
+            $word_count = str_word_count( strip_tags( $content ) );
+            $words_per_minute = 250;
+            if (get_option('wpm') != '') {
+                $words_per_minute = get_option('wpm');
+            }
+            $time_in_minutes = $word_count/$words_per_minute;
+
+            if ($time_in_minutes < 1) {
+                $full = '<span class="timespan less-than"><p>';
+                if (get_option('lt1') != '') {
+                    $full .= get_option('lt1'); // get the option from WP backend
+                } else {
+                    $full .= 'less than one minute.';
+                }
+                $full .= '</p></span>';
+                // $full .= strval($time_in_minutes);
+                return $full . $content;
+            } else {
+                $time_in_minutes = round($time_in_minutes); // round the time to a full number
+                $strTime = strval($time_in_minutes); // convert time to a string
+                $full = '<span class="timespan greater-than"><p>';
+                if (get_option('gt1') != '') {
+                    $full .= get_option('gt1'); // get the option from WP backend
+                } else {
+                    $full .= 'greater than';
+                }
+                $full .= '<span class="ts-time"> ' . $strTime . '</span> '; // add time onto the end of the content
+                if (get_option('gt1po') != '') {
+                    $full .= get_option('gt1po'); // get the option from WP backend
+                } else {
+                    $full .= 'minutes';
+                }
+                return $full . $content; // return rounded time
+            }
+        }
+    } else {
+        return $content;
+    }
+}
+add_filter('the_content', 'auto_TTR');
+
+?>
