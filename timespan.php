ca9dca8045c13c32b5f2e3ff64fb0ad51058c35a
paperleaf <paperleaf@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-20 18:59:58 +0000

    update to shortcode
    git-svn-id: https://plugins.svn.wordpress.org/timespan/trunk@1010829 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- timespan.php
+++ timespan.php
@@ -3,7 +3,7 @@
 Plugin Name: TimeSpan
 Plugin URI: http://paper-leaf.com
 Description: A simple, easy to use plugin which can be used to display the average time it will take to read a post.
-Version: 1.0.4
+Version: 1.0.5
 Author: Paper Leaf Design
 Author URI: http://paper-leaf.com
 License: GPLv2 or Later
@@ -39,40 +39,42 @@ function ttr_options() {
 
 
 function time_to_read() {
-    $content = get_post_field( 'post_content', $post->ID );
-    $word_count = str_word_count( strip_tags( $content ) );
-    $words_per_minute = 250;
-    if (get_option('wpm') != '') {
-        $words_per_minute = get_option('wpm');
-    }
-    $time_in_minutes = $word_count/$words_per_minute;
+    if ( is_single() && is_main_query() ) {
+        $content = get_post_field( 'post_content', $post->ID );
+        $word_count = str_word_count( strip_tags( $content ) );
+        $words_per_minute = 250;
+        if (get_option('wpm') != '') {
+            $words_per_minute = get_option('wpm');
+        }
+        $time_in_minutes = $word_count/$words_per_minute;
 
-    if ($time_in_minutes < 1) {
-        $full = '<span class="timespan less-than"><p>';
-        if (get_option('lt1') != '') {
-            $full .= get_option('lt1'); // get the option from WP backend
-        } else {
-            $full .= 'less than one minute.';
-        }
-        $full .= '</p></span>';
-        return $full;
-    } else {
-        $time_in_minutes = round($time_in_minutes); // round the time to a full number
-        $strTime = strval($time_in_minutes); // convert time to a string
-        $full = '<span class="timespan greater-than"><p>';
-        if (get_option('gt1') != '') {
-            $full .= get_option('gt1'); // get the option from WP backend
-        } else {
-            $full .= 'greater than';
-        }
-        $full .= '<span class="ts-time"> ' . $strTime . '</span> '; // add time onto the end of the content
-        if (get_option('gt1po') != '') {
-            $full .= get_option('gt1po'); // get the option from WP backend
+        if ($time_in_minutes < 1) {
+            $full = '<span class="timespan less-than"><p>';
+            if (get_option('lt1') != '') {
+                $full .= get_option('lt1'); // get the option from WP backend
+            } else {
+                $full .= 'less than one minute.';
+            }
             $full .= '</p></span>';
+            return $full;
         } else {
-            $full .= 'minutes </p></span>';
+            $time_in_minutes = round($time_in_minutes); // round the time to a full number
+            $strTime = strval($time_in_minutes); // convert time to a string
+            $full = '<span class="timespan greater-than"><p>';
+            if (get_option('gt1') != '') {
+                $full .= get_option('gt1'); // get the option from WP backend
+            } else {
+                $full .= 'greater than';
+            }
+            $full .= '<span class="ts-time"> ' . $strTime . '</span> '; // add time onto the end of the content
+            if (get_option('gt1po') != '') {
+                $full .= get_option('gt1po'); // get the option from WP backend
+                $full .= '</p></span>';
+            } else {
+                $full .= 'minutes </p></span>';
+            }
+            return $full; // return rounded time
         }
-        return $full; // return rounded time
     }
 }
 add_shortcode('time-span', 'time_to_read');
